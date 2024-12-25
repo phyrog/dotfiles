ea26b89dd7956d61d8eff3acb99fd6d72e678051
Tom Gehrke <phyrog@gmail.com>
2024-12-25 20:45:32 +0100

    configure tree-sitter

--- dot_config/nvim/lua/plugins/tree-sitter.lua
+++ dot_config/nvim/lua/plugins/tree-sitter.lua
@@ -1,3 +1,27 @@
 return {
-  { 'nvim-treesitter/nvim-treesitter', name = 'tree-sitter' }
+  {
+    'nvim-treesitter/nvim-treesitter',
+    name = 'tree-sitter',
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = {
+            "bash", "make", "diff",
+            "dockerfile",
+            "gitignore",
+            "go", "gomod", "gosum", "proto", "rust", "sql", "zig",
+            "html", "javascript", "typescript",
+            "json", "toml", "yaml",
+            "lua", "vim",
+            "ssh_config",
+            "typst",
+          },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+  }
 }
