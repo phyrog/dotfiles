951faedd4dd81cf078404835641c71736dee85a0
Tom Gehrke <t.gehrke@reply.de>
2025-01-02 09:21:21 +0100

    run antidote bundle after antidote update

--- run_after_antidote-update.sh
+++ run_after_antidote-update.sh
@@ -3,4 +3,8 @@
 fpath+=(${ZDOTDIR:-~}/.antidote)
 autoload -Uz $fpath[-1]/antidote
 
+zsh_plugins=${ZDOTDIR:-~}/.zsh_plugins.zsh
+
 antidote update
+
+antidote bundle <${zsh_plugins:r}.txt >|$zsh_plugins
